e3e52755d4fe58625bf2677cd3b781bfb0d8bb32
J3FF97 <jefri@quicknet.nl>
2014-07-15 21:30:47 +0200

    Added Thaumcraft Compat, moved mods to seperate package, Updated .gitignore to ignore idea files.
    This was the best way I could think of doing this, if there is a better way, please let me know ^.^

--- src/main/java/spatialiocompat/core/SIOCompat.java
+++ src/main/java/spatialiocompat/core/SIOCompat.java
@@ -12,66 +12,37 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 public class SIOCompat 
 {
 
-	public final static String modid = "spatialio-compat";
-	public final static String name = "Spatial IO Compat";
-	public final static String version = "@version@";
-	public final static String dependencies ="required-after:appliedenergistics2";
+    public final static String modid        = "spatialio-compat";
+    public final static String name         = "Spatial IO Compat";
+    public final static String version      = "@version@";
+    public final static String dependencies = "required-after:appliedenergistics2";
 
-	private SIOCompatMarker Marker = new SIOCompatMarker();
+    private static SIOCompatMarker Marker = new SIOCompatMarker();
 
-	public SIOCompat()
-	{
-		// Mark in the crash logs that this is installed visibly.
-		FMLCommonHandler.instance().registerCrashCallable( Marker );		
-	}
+    public SIOCompat()
+    {
+        // Mark in the crash logs that this is installed visibly.
+        FMLCommonHandler.instance().registerCrashCallable(Marker);
+    }
 
-	void addTileEntity( String tileEntityPath )
-	{
-		Marker.add( tileEntityPath );
-		FMLInterModComms.sendMessage( "appliedenergistics2", "whitelist-spatial", tileEntityPath );
-	}
+    public static void addTileEntity(String tileEntityPath)
+    {
+        Marker.add(tileEntityPath);
+        FMLInterModComms.sendMessage("appliedenergistics2", "whitelist-spatial", tileEntityPath);
+    }
 
-	@EventHandler
-	void PreInit(FMLPreInitializationEvent event)
-	{
-		// nothing.
-	}
+    @EventHandler
+    void PreInit(FMLPreInitializationEvent event)
+    {
+        // nothing.
+    }
 
-	@EventHandler
-	void Init(FMLInitializationEvent event)
-	{
-
-		/**
-		 * Carpenters Blocks
-		 *
-		 * Tested by AlgorithmX2 - no issues found.
-		 * Friday, June 20, 2014
-		 *
-		 * Includes all Customizable Blocks
-		 */
-		addTileEntity( "carpentersblocks.tileentity.TEBase" );
-
-		/**
-		 * Iron Chests
-		 *
-		 * Tested by AlgorithmX2 - no issues found.
-		 * Friday, June 20, 2014
-		 *
-		 * Includes All Types of Chests
-		 */
-		addTileEntity( "cpw.mods.ironchest.TileEntityIronChest" );
-
-        /**
-         * Thaumcraft
-         *
-         * Tested by J3FF97 - no issues found.
-         * Tuesday, July 15, 2014
-         *
-         * Includes everything. Aspects are retained, everything still functions.
-         * NOTE: Did not test on multiblocks. Do not try them if you don't want to lose your stuff. (High probability of not working/crashing)
-         */
-        addTileEntity("thaumcraft.common.tiles.TileThaumcraft");
-
+    @EventHandler
+    void Init(FMLInitializationEvent event)
+    {
+        SIOCCarpenterBlocks.RegisterCompat();
+        SIOCIronChests.RegisterCompat();
+        SIOCThaumcraft.RegisterCompat();
 
     }
 
