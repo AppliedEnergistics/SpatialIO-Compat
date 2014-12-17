cb44846494fe222982ddfee91a3264f72e00c5f2
FireBall1725 <treed@sunnking.com>
2014-12-16 20:27:32 -0500

    Added OpenModsLib

---
+++ src/main/java/spatialiocompat/modules/OpenModsLib.java
@@ -0,0 +1,10 @@
+package spatialiocompat.modules;
+
+import spatialiocompat.core.SIOBaseModule;
+
+/**
+ * Created by FireBall1725 on 12/16/2014.
+ */
+public class OpenModsLib extends SIOBaseModule {
+    public static void register() { addTileEntity("openmods.tileentity.OpenTileEntity"); }
+}
